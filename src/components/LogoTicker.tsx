53d01ed4a9095fe300d747274e140ae9d9cb93d5
Mukesh Kumar <mk5504472@gmail.com>
2024-08-14 23:32:20 +0530

    css styling

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -22,8 +22,9 @@ export const LogoTicker = () => {
   return (
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2>Trusted by the world&apos;s most innovative teams</h2>
-
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world&apos;s most innovative teams
+        </h2>
         <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:z-10 before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
           <motion.div
             className="flex gap-16 flex-none pr-16"
